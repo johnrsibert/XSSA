bf0ed910e493ebcdfaafde74ee8935f3ccc04217
John Sibert <sibert@hawaii.edu>
2015-01-09 13:31:23 -1000

    clean build on TMB framework

--- TMB/xssams.R
+++ TMB/xssams.R
@@ -2,118 +2,93 @@ require(TMB)
 compile("xssams.cpp")
 dyn.load(dynlib("xssams"))
 
-   get.field = function(p)
+field.counter <<- 0
+
+   get.field = function()
    {
-     field = sca[p]
+     field.counter <<- field.counter + 1
+     field = sca[field.counter]
+   # print(paste(field.counter,field))
      return(field)
    }
    
-   get.numeric.field<-function(p)
+   get.numeric.field<-function()
    {
-      ret = as.numeric(get.field(p))
+      ret = as.numeric(get.field())
       return(ret)
    }
 
-sca = scan(file="xssams.dat",comment.char="#",what="raw",quiet=TRUE)
-catch.file.name = get.field(1)
-forcing.file.name = get.field(2)
-ntime = get.numeric.field(3)
-ngear = get.numeric.field(4)
-dt = get.numeric.field(5)
+dat.file = "xssams.dat"
+sca = scan(file=dat.file,comment.char="#",what="raw",quiet=TRUE)
+print(paste("Read",length(sca),"items from ",dat.file))
+data = list()
 
-data = structure(list(
-ntime = ntime,
-ngear = ngear,
-dt = dt,
-fr = get.numeric.field(6),
-#init.log.T12 = get.numeric.field(7),
-#phase.logT12 = get.numeric.field(8),
-#init.logT21 = get.numeric.field(9),
-#phase.logT21 = get.numeric.field(10) ,
-#init.logr = get.numeric.field(11),
-#phase.logr = get.numeric.field(12),
-#init.logK = get.numeric.field(13),
-#phase.logK = get.numeric.field(14),
-#init.logsdlogF=vector(length=ngear),
-#phase.logsdlogF = -1,
-#init.logsdlogPop = vector(length=2),
-#phase.logsdlogPop = -1,
-#init.logsdlogYield=vector(length=ngear),
-#phase.logsdlogYield = -1,
-#init.LmeanProportion.local = 0.0,
-#phase.LmeanProportion.local = -1,
-#init.logsdLProportion.local = 0.0,
-#phase.logsdLProportion.local = -7,
-ObsCatch=matrix(nrow=ngear,ncol=ntime),
-ImmigrantBiomass=vector(length=ntime),
-maxtime = ntime,
-lengthU = ntime*(ngear+2),
-ss = 1.0/dt,
-# set up U indexing
-#Fndxl = vector(length=ntime),
-#Fndxu = vector(length=ntime),
-Fndxl = seq(0,(ntime-1)*(ngear),ngear)
-Fndxu = seq(0,(ntime-1)*(ngear),ngear)+(ngear-1)
-utPop1 = ngear*ntime,
-utPop2 = ngear*ntime + ntime
-))
+catch.file.name = get.field()
+forcing.file.name = get.field()
 
-nn = 14
-#for (g in 1:ngear)
-#{
-   #nn = nn+1
-   #data$init.logsdlogF[g] = get.numeric.field(nn)
-#}
-#nn = nn+1
-#data$phase.logsdlogF = get.numeric.field(nn)
+data$ntime = get.numeric.field()
+data$ngear = get.numeric.field()
+data$dt = get.numeric.field()
+data$fr = get.numeric.field()
+data$init.logT12 = get.numeric.field()
+data$phase.logT12 = get.numeric.field()
+data$init.logT21 = get.numeric.field()
+data$phase.logT21 = get.numeric.field() 
+data$init.logr = get.numeric.field()
+data$phase.logr = get.numeric.field()
+data$init.logK = get.numeric.field()
+data$phase.logK = get.numeric.field()
 
-#nn = nn+1
-#data$init.logsdlogPop[1] = get.numeric.field(nn)
-#nn = nn+1
-#data$init.logsdlogPop[2] = get.numeric.field(nn)
-#nn = nn+1
-#data$phase.logsdlogPop = get.numeric.field(nn)
-#for (g in 1:ngear)
-#{
-   #nn = nn+1
-   #data$init.logsdlogYield[g] = get.numeric.field(nn)
-#}
-#nn = nn+1
-#data$phase.logsdlogYield = get.numeric.field(nn)
-#nn = nn+1
-#data$init.LmeanProportionLocal = get.numeric.field(nn)
-#nn = nn+1
-#data$phase.LmeanProportionLocal = get.numeric.field(nn)
-#nn = nn+1
-#data$init.logsdLProportion.local = get.numeric.field(nn)
-#nn = nn+1
-#data$phase.logsdLProportionLocal = get.numeric.field(nn)
+ntime=data$ntime
+ngear=data$ngear
 
-for ( t in 1:ntime)
-{
-   data$Fndxl[t] = (t-1)*ngear+1
-   data$Fndxu[t] = t*ngear
-}
-
-#print(nn)
-#print(data$phase.logsdLProportion.local)
+data$init.logsdlogF=vector(length=ngear)
+for (g in 1:ngear)
+   data$init.logsdlogF[g] = get.numeric.field()
+data$phase.logsdlogF = get.numeric.field()
+data$init.logsdlogPop = get.numeric.field()
+data$phase.logsdlogPop = get.numeric.field()
+data$init.logsdlogYield=vector(length=ngear)
+for (g in 1:ngear)
+   data$init.logsdlogYield[g] = get.numeric.field()
+data$phase.logsdlogYield = get.numeric.field()
+data$init.LmeanProportion.local =  get.numeric.field()
+data$phase.LmeanProportion.local = get.numeric.field()
+data$init.logsdLProportion.local = get.numeric.field()
+data$phase.logsdLProportion.local = get.numeric.field()
+data$use.robustF = get.numeric.field()
+data$init.Lpfat = vector(length=ngear)
+for (g in 1:ngear)
+   data$init.Lpfat[g] = get.numeric.field()
+data$phase.pfat = get.numeric.field()
+print(paste(field.counter,"input fields processed"))
 
 data$ImmigrantBiomass = as.matrix(read.table(file=forcing.file.name))[data$fr,]
 data$ObsCatch = as.matrix(read.table(file=catch.file.name))
-#rm(list=c("sca","g","ngear","nn","ntime","dt","t"))
 
-parameters = list(
-logT12 = -12.0,
-logT21 = -12.0,
-logr = -2.3026,
-logK = 12.207,
-logsdlogF = rep(1.1,data$ngear),
-logsdlogPop = c(1.2,1.3),
-logsdlogYield=rep(1.4,data$ngear),
-LmeanProportionLocal = 2.1972,
-logsdLProportionLocal = 2.0,
-U = vector(length=data$lengthU,mode="numeric")
-)
+data$maxtime = ntime
+data$lengthU = ntime*(ngear+2)
+data$ss = 1.0/data$dt
+# set up U indexing starting at 0
+data$Fndxl = seq(0,(ntime-1)*(ngear),ngear)
+data$Fndxu = data$Fndxl+(ngear-1) 
+data$utPop1 = ngear*ntime - 1
+data$utPop2 = data$utPop1 + ntime
+
+
+parameters = list()
+parameters$logT12 = data$init.logT12
+parameters$logT21 = data$init.logT21
+parameters$logr = data$init.logr
+parameters$logK = data$init.logK
+parameters$logsdlogF = data$init.logsdlogF
+parameters$logsdlogPop = data$init.logsdlogPop
+parameters$logsdlogYield = data$init.logsdlogYield
+parameters$LmeanProportionLocal = data$init.LmeanProportion.local
+parameters$logsdLProportionLocal = data$init.logsdLProportion.local
+parameters$Lpfat = data$init.Lpfat
+parameters$U = vector(length=data$lengthU,mode="numeric")
+
 parameters$U=rep(0.0,data$lengthU)
 
-obj <- MakeADFun(data,parameters,random=c("U"),DLL="xssams")
+obj = MakeADFun(data,parameters,random=c("U"),DLL="xssams")
--- TMB/xssams.cpp
+++ TMB/xssams.cpp
@@ -1,4 +1,7 @@
 #include <TMB.hpp>
+#include <iostream>
+//#include "trace.h"
+//ofstream clogf("xssams.log");
 
 template <class Type> 
 Type square(Type x){return x*x;}
@@ -37,40 +40,47 @@ Type objective_function<Type>::operator() ()
    DATA_INTEGER(maxtime);
    DATA_FACTOR(Fndxl);
    DATA_FACTOR(Fndxu);
+   //std::cout << Fndxu << std::endl;
+   //TRACE(Fndxu)
 
    PARAMETER(logT12);
    PARAMETER(logT21);
    PARAMETER(logr);
    PARAMETER(logK);
    PARAMETER_VECTOR(logsdlogF);
-   PARAMETER_VECTOR(logsdlogPop);
+   PARAMETER(logsdlogPop);
    PARAMETER_VECTOR(logsdlogYield)
    PARAMETER(LmeanProportionLocal);
    PARAMETER(logsdLProportionLocal);
+   PARAMETER_VECTOR(Lpfat);
 
    PARAMETER_VECTOR(U);
    Type r = exp(logr);
    Type K = exp(logK);
    vector <Type> varlogF = square(exp(logsdlogF));
    vector <Type>  sdlogF = exp(logsdlogF);
-   vector <Type> varlogPop = square(exp(logsdlogPop));
+   Type varlogPop = square(exp(logsdlogPop));
    vector <Type> varlogYield = square(exp(logsdlogYield));
    Type T12 = exp(logT12);
    Type T21 = exp(logT21);
 
    Type nll = 0.0;
-   for (int t = 0; t < ntime; t++)
+   using namespace density;
+   for (int t = 1; t < ntime; t++)
    {
       for (int g = 0; g < ngear; g++)
       {
-         nll += -dnorm(U(Fndxl(t-1)+g)-U(fndxl(t)+g),sdlogF);
+         Type Fdiff = U(Fndxl(t-1)+g)-U(Fndxl(t)+g);
+         Type Fsd = sdlogF(g);
+         nll += square(Fdiff/Fsd);
+      //   nll -= dnorm(Fdiff,Fsd,true);
+      // nll += -density::dnorm(U(Fndxl(t-1)+g)-U(Fndxl(t)+g),sdlogF(g));
       }
 
    }
 
 
 
-   }
 
    return nll;
 }
