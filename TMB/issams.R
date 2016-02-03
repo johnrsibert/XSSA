5e004b74505ff4cbace1d9866c654ce400ad113a
John Sibert <sibert@hawaii.edu>
2016-02-02 15:29:16 -1000

    begin port issams.tpl to TMB

---
+++ TMB/issams.R
@@ -0,0 +1,205 @@
+require(TMB)
+compile("issams.cpp",flags="-O0 -g",safebounds=TRUE,safeunload=TRUE)
+dyn.load(dynlib("issams"))
+
+field.counter <<- 0
+
+   get.field = function()
+   {
+     field.counter <<- field.counter + 1
+     field = sca[field.counter]
+   # print(paste(field.counter,field))
+     return(field)
+   }
+   
+   get.numeric.field<-function()
+   {
+      ret = as.numeric(get.field())
+      return(ret)
+   }
+
+logit<-function(p)
+{
+   return(log(p/(1-p)))
+}
+
+alogit<-function(alpha)
+{
+   return(1/(1+exp(-alpha)))
+}
+
+
+dat.file = "./issams.dat"
+sca = scan(file=dat.file,comment.char="#",what="raw",quiet=TRUE)
+print(paste("Read",length(sca),"items from ",dat.file))
+
+data = list()
+phases = list()
+
+data$ngear = get.numeric.field()
+nobs.gear=data$ngear
+data$ntime = get.numeric.field()
+ntime=data$ntime
+data$dt = get.numeric.field()
+print(paste(nobs.gear,ntime,data$dt))
+tcatch=matrix(nrow=nobs.gear,ncol=ntime)
+#data$obs.catch=matrix(nrow=nobs.gear,ncol=ntime)
+print(dim(data$obs.catch))
+for (g in 1:nobs.gear)
+{
+   for (t in 1:ntime)
+   {
+      tcatch[g,t] = get.numeric.field()
+   }
+}
+data$use.klingons=get.numeric.field()
+data$use.klingon.multiplier=get.numeric.field()
+
+nzero = ntime;
+ziter = 0;
+while (nzero > 0)
+{
+   ziter = ziter + 1
+   nzero = 0;
+   for (g in 1:nobs.gear)
+      for (t in 2:ntime)
+         if ( (tcatch[g,t] <= 0.0) 
+              && (tcatch[g,t-1] > 0.0) && (tcatch[g,t+1] > 0.0) )
+         {
+            nzero = nzero + 1 
+            print(paste(nzero,ziter))
+            tcatch[g,t] = 0.5*(tcatch[g,t-1] + tcatch[g,t+1])
+            print(paste(nzero, " catch for gear ", g ," at time ", t,
+                  " set to ", data$obs.catch[g,t],sep=""))
+         }
+}
+print(paste("Zero catch bridging instances:", nzero))
+ZeroCatch = 1.0
+data$obs_catch = t(log(tcatch+ZeroCatch))
+
+
+forcing.matrix=matrix(nrow=9,ncol=data$ntime)
+for (r in 1:9)
+{
+   for (y in 1:ntime)
+   {
+      forcing.matrix[r,y] = get.numeric.field()
+   }
+}
+data$fr = get.numeric.field()
+data$immigrant_biomass = forcing.matrix[data$fr,]
+
+data$use_mean_forcing = get.numeric.field()
+mean.immigrant.biomass = mean(forcing.matrix[data$fr]);
+maximum.immigrant.biomass = max(forcing.matrix[data$fr]);
+if (data$use_mean_forcing)
+   data$immigrant_biomass = mean.immigrant.biomass;
+print(data$immigrant.biomass)
+
+
+data$phase_Fmsy = get.numeric.field()
+phases = c(phases,data$phase_Fmsy)
+data$init_Fmsy = get.numeric.field()
+
+data$use_r_prior = get.numeric.field()
+r.prior = get.numeric.field()
+data$logr_prior = log(r.prior)
+sdr.prior = get.numeric.field()
+data$varr_prior = sdr.prior*sdr.prior
+
+data$phase_MSY = get.numeric.field()
+phases = c(phases,data$phase_MSY)
+data$init_MSY = get.numeric.field()
+
+data$phase_sdlogProc = get.numeric.field()
+phases = c(phases,data$phase_sdlogProc)
+data$init_sdlogProc = get.numeric.field()
+
+data$phase_sdlogYield = get.numeric.field()
+phases = c(phases,data$phase_sdlogYield)
+data$init_sdlogYield = get.numeric.field()
+
+data$use_Q = get.numeric.field()
+data$phase.Q = get.numeric.field()
+phases = c(phases,data$phase_qProp)
+data$init_Q = get.numeric.field()
+
+data$use_robustY = get.numeric.field()
+data$phase_pcon = get.numeric.field()
+phases = c(phases,data$phase_pcon)
+data$init_pcon = get.numeric.field()
+
+print(paste(field.counter,"input fields processed"))
+
+#data$maxtime = ntime
+data$lengthU = ntime*(nobs.gear+1)
+# set up U indexing starting at 0
+data$Fndxl = seq(0,(ntime-1)*(nobs.gear),nobs.gear)
+data$Fndxu = data$Fndxl+(nobs.gear-1) 
+data$utPop = nobs.gear*ntime - 1
+
+
+parameters = list(
+  logFmsy = log(data$init_Fmsy),
+  logMSY = log(data$init_MSY),
+  logsdlogProc = log(data$init_sdlogProc),
+  logsdlogYield = log(data$init_sdlogYield),
+  logQ = log(data$init_Q),
+  Lpcon = data$init_pcon
+)
+
+if (!data$use_robustY)
+{
+   data$phase_pcon = -1;
+   data$init_pcon = 1e-25
+}
+parameters$Lpcon = logit(data$init_pcon)
+
+print(data)
+print(parameters)
+
+parameters$U=rep(0.0,data$lengthU)
+
+phases=unlist(phases) # phase flag for each parameter
+nap = length(phases)  # number of parameters
+print(paste("number of parameters:",nap))
+nphase = max(phases)  # number of phases
+print(paste("number of phases",nphase))
+fit.par=vector(length=nphase) # parameters estimates from each fit
+
+for (p in 1:nphase) # loop through phases
+{
+   map = list() # name list of factors
+   nip = 0
+   for (n in 1:nap) # loop through parameters
+   {
+      # check phase flag for this parameter
+      if ((phases[n] == -1) || (phases[n] > p))
+      {
+         map.entry = parameters[n]
+         map = c(map,map.entry)
+         nip = nip+1
+	 map[[nip]]=rep(factor(NA),length(parameters[[n]]))
+      }
+   }
+   print(paste("----------phase",p))
+   print(map)
+#  build model for active parameters, omiting thos in map
+   if (p == 1)
+   {
+      obj = MakeADFun(data,parameters,map=map,random=c("U"),DLL="issams")
+   }
+   else
+   {
+      obj = MakeADFun(data,opt.par[p-1],map=map,random=c("U"),DLL="issams")
+   }
+   lower <- obj$par*0-Inf
+   upper <- obj$par*0+Inf
+   opt = nlminb(obj$par,obj$fn,obj$gr,lower=lower,upper=upper)
+   # save parameter estimates for use in next fit
+   opt.par[p] = obj$env$parList(opt$par) 
+}
+
+#system.time(opt<-nlminb(obj$par,obj$fn,obj$gr,lower=lower,upper=upper))
+#rep<-sdreport(obj)
+#rep
